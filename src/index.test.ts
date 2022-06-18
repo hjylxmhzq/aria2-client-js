fd7122c953d9d1ac5b08a9e3f066dc15e5bfdb27
Tony Hu <hujingyuan25@163.com>
2022-06-18 22:23:04 +0800

    feat: add post/get/ws support

---
+++ src/index.test.ts
@@ -0,0 +1,46 @@
+import Aria2 from ".";
+
+const aria2UsePost = new Aria2({
+    url: 'http://127.0.0.1:6800/jsonrpc', // your aria2 api url and token
+    token: '12345678',
+    transportType: 'post',
+});
+const aria2UseGet = new Aria2({
+    url: 'http://127.0.0.1:6800/jsonrpc', // your aria2 api url and token
+    token: '12345678',
+    transportType: 'get',
+});
+const aria2UseWS = new Aria2({
+    url: 'ws://127.0.0.1:6800/jsonrpc', // your aria2 api url and token
+    token: '12345678',
+    transportType: 'websocket',
+});
+
+test('getGlobalOption:GET', async () => {
+    let aria2 = aria2UseGet;
+    const globalOptions = await aria2.getGlobalOption();
+    expect(typeof globalOptions === 'object').toBeTruthy();
+    const allKeys = new Set(Object.keys(globalOptions));
+    expect(globalOptionKeys.every(k => allKeys.has(k))).toBeTruthy();
+    aria2.destroy();
+});
+
+test('getGlobalOption:POST', async () => {
+    let aria2 = aria2UsePost;
+    const globalOptions = await aria2.getGlobalOption();
+    expect(typeof globalOptions === 'object').toBeTruthy();
+    const allKeys = new Set(Object.keys(globalOptions));
+    expect(globalOptionKeys.every(k => allKeys.has(k))).toBeTruthy();
+    aria2.destroy();
+});
+
+test('getGlobalOption:WebSocket', async () => {
+    let aria2 = aria2UseWS;
+    const globalOptions = await aria2.getGlobalOption();
+    expect(typeof globalOptions === 'object').toBeTruthy();
+    const allKeys = new Set(Object.keys(globalOptions));
+    expect(globalOptionKeys.every(k => allKeys.has(k))).toBeTruthy();
+    aria2.destroy();
+});
+
+const globalOptionKeys = ["allow-overwrite", "allow-piece-length-change", "always-resume", "async-dns", "auto-file-renaming", "auto-save-interval", "bt-detach-seed-only", "bt-enable-hook-after-hash-check", "bt-enable-lpd", "bt-force-encryption", "bt-hash-check-seed", "bt-load-saved-metadata", "bt-max-open-files", "bt-max-peers", "bt-metadata-only", "bt-min-crypto-level", "bt-prioritize-piece", "bt-remove-unselected-file", "bt-request-peer-speed-limit", "bt-require-crypto", "bt-save-metadata", "bt-seed-unverified", "bt-stop-timeout", "bt-tracker", "bt-tracker-connect-timeout", "bt-tracker-interval", "bt-tracker-timeout", "ca-certificate", "check-certificate", "check-integrity", "conditional-get", "conf-path", "connect-timeout", "console-log-level", "content-disposition-default-utf8", "continue", "daemon", "deferred-input", "dht-entry-point", "dht-entry-point6", "dht-file-path", "dht-file-path6", "dht-listen-port", "dht-message-timeout", "dir", "disable-ipv6", "disk-cache", "download-result", "dry-run", "dscp", "enable-color", "enable-dht", "enable-dht6", "enable-http-keep-alive", "enable-http-pipelining", "enable-mmap", "enable-peer-exchange", "enable-rpc", "event-poll", "file-allocation", "follow-metalink", "follow-torrent", "force-save", "ftp-pasv", "ftp-reuse-connection", "ftp-type", "hash-check-only", "help", "http-accept-gzip", "http-auth-challenge", "http-no-cache", "http-want-digest", "human-readable", "keep-unfinished-download-result", "listen-port", "log-level", "lowest-speed-limit", "max-concurrent-downloads", "max-connection-per-server", "max-download-limit", "max-download-result", "max-file-not-found", "max-mmap-limit", "max-overall-download-limit", "max-overall-upload-limit", "max-resume-failure-tries", "max-tries", "max-upload-limit", "metalink-enable-unique-protocol", "metalink-preferred-protocol", "min-split-size", "min-tls-version", "netrc-path", "no-conf", "no-file-allocation-limit", "no-netrc", "on-download-complete", "on-download-stop", "optimize-concurrent-downloads", "parameterized-uri", "pause-metadata", "peer-agent", "peer-id-prefix", "piece-length", "proxy-method", "quiet", "realtime-chunk-checksum", "remote-time", "remove-control-file", "retry-on-400", "retry-on-403", "retry-on-406", "retry-on-unknown", "retry-wait", "reuse-uri", "rlimit-nofile", "rpc-allow-origin-all", "rpc-listen-all", "rpc-listen-port", "rpc-max-request-size", "rpc-save-upload-metadata", "rpc-secure", "save-not-found", "save-session", "save-session-interval", "seed-ratio", "seed-time", "server-stat-timeout", "show-console-readout", "show-files", "socket-recv-buffer-size", "split", "stderr", "stop", "stream-piece-selector", "summary-interval", "timeout", "truncate-console-readout", "uri-selector", "use-head", "user-agent"];
---
+++ src/index.ts
@@ -0,0 +1,158 @@
+import { EventEmitter } from 'events';
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
+import { v4 as uuidv4 } from 'uuid'
+import { encode as encodeBase64 } from 'js-base64';
+import { WebSocket, MessageEvent } from 'ws';
+
+export type EventName = 'downloadStart';
+export type TransportType = 'get' | 'post' | 'websocket';
+
+type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
+type PartialRequired<T, K extends keyof T> = Required<Pick<T, K>> & Partial<Omit<T, K>>;
+
+export interface Aria2Options {
+    token: string;
+    transportType: TransportType;
+    url: string;
+}
+
+type OptionalKey = 'transportType';
+
+type Aria2ConstructorOptions = Optional<Aria2Options, OptionalKey>;
+
+const defaultOptions: PartialRequired<Aria2Options, OptionalKey> = {
+    transportType: 'post',
+}
+
+const httpAgent = axios.create();
+
+export default class Aria2 extends EventEmitter {
+    public options: Aria2Options;
+    public ax: AxiosInstance;
+    private _ws?: WebSocket;
+    private wsMessageListeners: { id: string, listener: (ev: MessageEvent) => void }[] = [];
+    constructor(_options: Aria2ConstructorOptions) {
+        super();
+        this.options = Object.assign({}, defaultOptions, _options);
+        this.ax = httpAgent;
+    }
+
+    destroy() {
+        if (this._ws) {
+            this._ws.removeEventListener('message', this._wsEventListener);
+            this._ws.close();
+        }
+    }
+
+    private _wsEventListener = (ev: MessageEvent) => {
+        const jsonData = JSON.parse(ev.data.toString());
+        const notUsed: typeof this.wsMessageListeners = [];
+        for (const listener of this.wsMessageListeners) {
+            if (listener.id === jsonData.id) {
+                listener.listener(jsonData);
+            } else {
+                notUsed.push(listener);
+            }
+        }
+        this.wsMessageListeners = notUsed;
+    }
+
+    private get ws(): Promise<WebSocket> {
+        if (!this._ws) {
+            this._ws = new WebSocket(this.options.url);
+            this._ws.addEventListener('message', this._wsEventListener);
+        }
+        return new Promise((resolve, reject) => {
+            if (this._ws!.readyState === WebSocket.OPEN) {
+                resolve(this._ws as WebSocket);
+            } else {
+                this._ws!.addEventListener('open', () => {
+                    resolve(this._ws as WebSocket);
+                }, { once: true });
+            }
+        });
+    }
+
+    private onWSMessage(id: string, fn: (msg: any) => void) {
+        this.wsMessageListeners.push({ listener: fn, id });
+    }
+
+    private uid() {
+        return uuidv4();
+    }
+
+    private async callByGet(method: string, params: any[]) {
+        const search = new URLSearchParams();
+
+        const allParams = [`token:${this.options.token}`, ...params];
+        search.append('method', method);
+        search.append('id', this.uid());
+        search.append('jsonrpc', '2.0');
+        search.append('params', encodeBase64(JSON.stringify(allParams)));
+
+        const queryString = this.options.url + '?' + search.toString();
+        const resp = await this.ax.get(queryString);
+        const result = resp.data.result;
+        return result;
+    }
+
+    private async callByPost(method: string, params: any[]) {
+        const resp = await this.ax.post(this.options.url, {
+            id: this.uid(),
+            jsonrpc: "2.0",
+            method,
+            params: [`token:${this.options.token}`, ...params],
+        });
+        const result = resp.data.result;
+        return result;
+    }
+
+    private async callByWS(method: string, params: any[]) {
+        const ws = await this.ws;
+        const uid = this.uid();
+        ws.send(JSON.stringify({
+            id: uid,
+            jsonrpc: "2.0",
+            method,
+            params: [`token:${this.options.token}`, ...params],
+        }));
+
+        return new Promise((resolve, reject) => {
+            this.onWSMessage(uid, (data) => {
+                resolve(data.result);
+            });
+        });
+    }
+
+    private async call(method: string, params: any[] = []) {
+        if (this.options.transportType === 'get') {
+            return await this.callByGet(method, params);
+        } else if (this.options.transportType === 'post') {
+            return await this.callByPost(method, params);
+        } else if (this.options.transportType === 'websocket') {
+            return await this.callByWS(method, params);
+        } else {
+            throw new Error(`unvalid transport type: ${this.options.transportType}`);
+        }
+    }
+
+    addURI(uri: string) {
+
+    }
+
+    async getGlobalOption() {
+        const result = await this.call('aria2.getGlobalOption');
+        return result;
+    }
+
+    on(eventName: 'downloadStart', listener: (file: string) => void): this;
+    on(eventName: EventName, listener: (...args: any[]) => void) {
+
+
+        return this;
+    }
+}
+
+export {
+    Aria2,
+};
